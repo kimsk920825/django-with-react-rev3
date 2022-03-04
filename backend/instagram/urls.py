4e7f210c30960a24d0773cac5cab877c5dbffd80
kimseungkyu <kimseungkyubestiedu@gmail.com>
2022-03-04 17:59:02 +0900

    feat: PublicListAPIView class made - PublicListAPIView and PostViewSet class both have same model - PublicListAPIView only creates post list - PostViewSet creates post list and post detail

--- backend/instagram/urls.py
+++ backend/instagram/urls.py
@@ -6,5 +6,6 @@ router = DefaultRouter()
 router.register("post", views.PostViewSet)  # 2개 url을 만들어줍니다.
 # router.urls에 리스트 형태로 url이 존재.
 urlpatterns = [
+    path("public/", views.PublicListAPIView.as_view()),
     path("", include(router.urls)),
 ]
--- backend/instagram/views.py
+++ backend/instagram/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+
+class PostViewSet(ModelViewSet):  # postlist, postdetail 5가지 모두 처리
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    # def dispatch(self, request, *args, **kwargs):
+    #     print("request.body : ", request.body)
+    #     print("request.POST : ", request.POST)
+    #     return super().dispatch(request, *args, **kwargs)
+
+
+class PublicListAPIView(generics.ListCreateAPIView):  # postlist 2가지만 처리
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
