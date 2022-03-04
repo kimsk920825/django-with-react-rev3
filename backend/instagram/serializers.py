0a1b3ae9002bfca02bad5af027b83ae362947116
kimseungkyu <kimseungkyubestiedu@gmail.com>
2022-03-04 18:43:56 +0900

    feat: how to request Posting username (method#2) - make a new class only for author

--- backend/instagram/serializers.py
+++ backend/instagram/serializers.py
@@ -1,15 +1,22 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Post
 
 
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["username", "email"]
+
+
 class PostSerializer(serializers.ModelSerializer):
-    username = serializers.ReadOnlyField(source="author.username")
+    author = AuthorSerializer()
 
     class Meta:
         model = Post
         fields = [
             "pk",
-            "username",
+            "author",
             "message",
             "created_at",
             "updated_at",
